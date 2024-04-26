b76b23f9ce505ddd46850bdda4c175bdb4caa38f
Harraz <abdelrahmanharraz35@gmail.com>
2024-04-26 14:26:23 +0300

    - adding dummy auth screen - adding flutter to the application - adding needed dependency -initializing Firebase

---
+++ lib/features/Auth/presentation/screens/auth_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AnonymousAuthScreen extends StatelessWidget {
+  const AnonymousAuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Container(
+        width: 100,
+        height: 100,
+        color: Colors.amber,
+      ),
+    );
+  }
+}
